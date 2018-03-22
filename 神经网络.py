d112b734017a28763e4d97571adcfd4727d92b5d
littleYaoWu <36840923+littleYaoWu@users.noreply.github.com>
2018-03-22 11:42:59 +0800

    Update 神经网络.py

--- 神经网络.py
+++ 神经网络.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 #神经网络
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, classification_report
@@ -15,10 +14,10 @@ data[data != 1] = 0
 x = data.iloc[:, :3].as_matrix().astype(int)
 y = data.iloc[:, 3].as_matrix().astype(int)
 
+#导入keras出错，需修改keras.json，将其中的tensorflow改为theano
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation #activation是逐元素计算的激活函数，Dense分层
 
-#此处有个报错，未找到方法
 model = Sequential() #建立模型
 model.add(Dense(input_dim=3, units=10)) #3个输入节点，10个隐藏节点
 model.add(Activation('relu')) #用relu函数作为激活函数，提升准确度
@@ -26,7 +25,7 @@ model.add(Dense(input_dim=10, units=1))
 model.add(Activation('sigmoid')) #由于是0-1输出，用sigmoid做激活函数
 
 #修正神经网络
-model.compile(loss='binary_crossentropy', optimizer='adam', class_mode='binary')
+model.compile(loss='binary_crossentropy', optimizer='adam')
 #编译模型，做的是二元分类所以指定损失函数binary_crossentropy，模式为binary
 #adam求解的方法
 model.fit(x, y, nb_epoch=1000, batch_size=10) #进行1000次的学习
