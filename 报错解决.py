3d14611a80b6ee5884be15bc176ad649f0b268d4
littleYaoWu <36840923+littleYaoWu@users.noreply.github.com>
2018-08-12 15:25:40 +0800

    Update 报错解决.py

--- 报错解决.py
+++ 报错解决.py
@@ -1,2 +1,12 @@
 # 读取报错的时候encoding='gbk',errors="ignore" 忽略错误
 data = open("xxxx.log", encoding='gbk',errors="ignore").read()
+
+
+#datan=pd.concat([data1,data2,data3,data4,data5],axis=1)    
+data1.to_csv("train_innoraml.csv") 
+# 保存数据报错
+# UnicodeEncodeError: ‘ascii’ codec can’t encode characters in position 0-1: ordinal not in range(128)
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
